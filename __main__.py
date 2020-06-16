456928d12dd365cdf19cf6f86b2bf49e162ba66d
Ariel Barcellos <barcellos.ariel@gmail.com>
2020-06-16 15:18:09 -0500

    unsaved changes

--- __main__.py
+++ __main__.py
@@ -2,6 +2,7 @@ import pandas as pd
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix
 
 ad_data = pd.read_csv('./data/advertising.csv')
 ad_data.head()
@@ -43,4 +44,20 @@ predictors = [
     'Daily Internet Usage',
     'Daily Time Spent on Site'
 ]
-X = ad_data[[]]
+X = ad_data[predictors]
+y = ad_data['Clicked on Ad']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.33,
+    random_state=101
+)
+
+logmodel = LogisticRegression()
+logmodel.fit(X_train, y_train)
+
+y_pred = logmodel.predict(X_test)
+
+print(classification_report(y_test, y_pred))
+print(confusion_matrix(y_test, y_pred))
