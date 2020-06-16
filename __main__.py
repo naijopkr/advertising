b02538dde9b5104f1c10e71c3c02f67e5b81d9cc
Ariel Barcellos <barcellos.ariel@gmail.com>
2020-06-16 14:57:27 -0500

    exploratory data analysis

--- __main__.py
+++ __main__.py
@@ -1,2 +1,29 @@
-if __name__ == '__main__':
-    print('Hello world')
+import pandas as pd
+import seaborn as sns
+
+ad_data = pd.read_csv('./data/advertising.csv')
+ad_data.head()
+ad_data.info()
+ad_data.describe()
+
+ad_data['Age'].hist(bins=40)
+
+sns.set_style('whitegrid')
+sns.jointplot(x='Age', y='Area Income', data=ad_data)
+
+sns.jointplot(
+    x='Age',
+    y='Daily Time Spent on Site',
+    data=ad_data,
+    color='red',
+    kind='kde'
+)
+
+sns.jointplot(
+    x='Daily Internet Usage',
+    y='Daily Time Spent on Site',
+    data=ad_data,
+    color='green'
+)
+
+sns.pairplot(data=ad_data, hue='Clicked on Ad')
